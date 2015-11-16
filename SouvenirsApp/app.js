24b163812a79c0ca54445fc41306ecf1c2940685
Thomas Segismont <tsegismo@redhat.com>
2015-11-16 10:49:50 +0100

    Client application skeleton

---
+++ SouvenirsApp/app.js
@@ -0,0 +1,10 @@
+var Router = require("vertx-web-js/router");
+
+var StaticHandler = require("vertx-web-js/static_handler");
+
+var router = Router.router(vertx);
+
+router.route().handler(StaticHandler.create().handle);
+
+var server = vertx.createHttpServer();
+server.requestHandler(router.accept).listen(8080);
