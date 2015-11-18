a0bd1f110532e43e58e4c5f5f413bddd3d96d394
Thomas Segismont <tsegismo@redhat.com>
2015-11-18 11:29:48 +0100

    Add details page with REST interaction and counter sending

--- SouvenirsApp/app.js
+++ SouvenirsApp/app.js
@@ -1,10 +1,26 @@
-var Router = require("vertx-web-js/router");
+var Router = require('vertx-web-js/router');
 
-var StaticHandler = require("vertx-web-js/static_handler");
+var StaticHandler = require('vertx-web-js/static_handler');
 
 var router = Router.router(vertx);
+
+var likes = 0;
+
+router.get('/rest/likes').produces('application/json').handler(function (routingContext) {
+  var response = routingContext.response();
+  response.putHeader('Content-Type', 'application/json');
+  response.end(JSON.stringify({'value': likes}));
+});
+
+router.post('/rest/likes/increment').produces('application/json').handler(function (routingContext) {
+  var response = routingContext.response();
+  response.putHeader('Content-Type', 'application/json');
+  response.end(JSON.stringify({'value': ++likes}));
+  vertx.eventBus().publish('metrics', {'source': 'app.souvenirs.likes', 'value': likes, 'type': 'counter'});
+});
 
 router.route().handler(StaticHandler.create().setFilesReadOnly(false).handle);
 
 var server = vertx.createHttpServer();
-server.requestHandler(router.accept).listen(8080);
+server.requestHandler(router.accept).listen(8181);
+
--- SouvenirsApp/webroot/js/souvenirs.js
+++ SouvenirsApp/webroot/js/souvenirs.js
@@ -1,35 +1,47 @@
 var souvenirsApp = angular.module('souvenirsApp', []);
 
-var items = [
-  {
-    'name': 'Gabian',
-    'snippet': 'Véritable Gabian de Riou.',
-    'image': {
-      'src': 'img/Larus_portrait.jpg',
-      'alt': 'Gabian de Riou'
-    },
-    'details': '#'
-  },
-  {
-    'name': 'Agrandisseur de sardines',
-    'snippet': 'L\'original. Fabriqué à Endoume.',
-    'image': {
-      'src': 'img/sardine.jpg',
-      'alt': 'Agrandisseur de sardines'
-    },
-    'details': '/agrandisseur-sardines.html'
-  },
-  {
-    'name': 'Camion à Pizza',
-    'snippet': 'Exemplaire unique. Très peu servi.',
-    'image': {
-      'src': 'img/camion-pizza.jpg',
-      'alt': 'Camion à Pizza'
-    },
-    'details': '#'
-  }
-];
-
 souvenirsApp.controller('ItemsCtrl', function ($scope) {
-  $scope.items = items;
+  $scope.items = [
+    {
+      'name': 'Gabian',
+      'snippet': 'Véritable Gabian de Riou.',
+      'image': {
+        'src': 'img/Larus_portrait.jpg',
+        'alt': 'Gabian de Riou'
+      },
+      'details': '#'
+    },
+    {
+      'name': 'Agrandisseur de sardines',
+      'snippet': 'L\'original. Fabriqué à Endoume.',
+      'image': {
+        'src': 'img/sardine.jpg',
+        'alt': 'Agrandisseur de sardines'
+      },
+      'details': '/agrandisseur-sardines.html'
+    },
+    {
+      'name': 'Camion à Pizza',
+      'snippet': 'Exemplaire unique. Très peu servi.',
+      'image': {
+        'src': 'img/camion-pizza.jpg',
+        'alt': 'Camion à Pizza'
+      },
+      'details': '#'
+    }
+  ];
+});
+
+souvenirsApp.controller('LikesCtrl', function ($scope, $http) {
+  $scope.likes = 0;
+
+  $http.get('rest/likes').success(function (data) {
+    $scope.likes = data.value;
+  });
+
+  $scope.increment = function () {
+    $http.post('rest/likes/increment').success(function (data) {
+      $scope.likes = data.value;
+    });
+  }
 });
