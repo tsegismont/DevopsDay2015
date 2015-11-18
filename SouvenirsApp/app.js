ee66e2bfb75a5788d1cd689a03352ae837cc8761
Thomas Segismont <tsegismo@redhat.com>
2015-11-18 15:12:36 +0100

    Push likes counter periodically

--- SouvenirsApp/app.js
+++ SouvenirsApp/app.js
@@ -16,6 +16,9 @@ router.post('/rest/likes/increment').produces('application/json').handler(functi
   var response = routingContext.response();
   response.putHeader('Content-Type', 'application/json');
   response.end(JSON.stringify({'value': ++likes}));
+});
+
+vertx.setTimer(5000, function () {
   vertx.eventBus().publish('metrics', {'source': 'app.souvenirs.likes', 'value': likes, 'type': 'counter'});
 });
 
